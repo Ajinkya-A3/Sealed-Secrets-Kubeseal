2282749f855ca836e18adad009c64f8517864729
Ajinkya-A3 <ajinkyaa3xd@gmail.com>
2026-01-30 05:56:32 +0000

    version changed and fixed

--- terraform-helm-sealed-secrets/versions.tf
+++ terraform-helm-sealed-secrets/versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.25"
+      version = "3.0.1"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.13"
+      version = "3.1.1"
     }
   }
 }
